9ceddd0b63b39cce858d7ee3ea5254432bf6063e
Akash Tiwari <akast2041@gmail.com>
2025-09-06 20:00:48 +0530

    feat(user) : add, update, and delete api

--- src/app.js
+++ src/app.js
@@ -2,17 +2,10 @@ const express = require('express');
 const app = express();
 const connectDb = require('./config/database');
 const User = require('./models/user');
+app.use(express.json());
 
 app.post('/signup', async (req, res) => {
-    const userObj = {
-        firstName: 'Akash',
-        lastName: 'Tiwari',
-        emailId: 'akash@yopmail.com',
-        password: '1234',
-        age: '25',
-        gender: 'male'
-    }
-    const user = new User(userObj);
+    const user = new User(req.body);
     try {
         await user.save();
         res.send('User added successfully!!')
@@ -20,6 +13,57 @@ app.post('/signup', async (req, res) => {
     catch (err) {
         res.status(400).send('Error saving the user:' + err.Message)
     }
+})
+
+app.get('/feed', async (req, res) => {
+
+    try{
+        const allUsers = await User.find({});
+        res.send(allUsers);
+    }
+    catch (err) {
+        res.status(400).send('Error saving the user:' + err.Message)
+    }
+})
+
+app.delete('/user', async (req, res) => {
+    const userId = req.body.userId;
+    
+    try{
+        const getUserId = await User.findByIdAndDelete(userId);
+        res.send('User deleted successfully!!');
+    }
+    catch (err) {
+        res.status(400).send('Error deleting the user:' + err.Message)
+    }
+})
+
+app.patch('/user', async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body
+    try{
+        const getUserId = await User.findByIdAndUpdate({_id : userId}, data);
+        res.send('User updated successfully!!');
+    }
+    catch (err) {
+        res.status(400).send('Something went wrong:' + err.Message)
+    }
+})
+
+app.get('/user', async (req, res) => {
+    const userEmail = req.body.emailId;
+    try {
+        const users = await User.find({ emailId: userEmail });
+        if(users.length === 0){
+            res.status(404).send('User not found!!')
+        }else {
+            res.send(users)
+        }
+    }
+    catch {
+        console.log('Something went wrong!!')
+    }
+
 })
 
 connectDb().then(() => {
