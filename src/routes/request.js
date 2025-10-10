1e5499f39a5ead4cb7349d5452ab01ce2f527872
Akash <akast2041@gmail.com>
2025-10-10 16:01:03 +0530

    created api to see connections list

--- src/routes/request.js
+++ src/routes/request.js
@@ -81,5 +81,31 @@ requestRouter.post('/request/review/:status/:requestId', userAuth, async (req, r
     }
 });
 
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
+
+requestRouter.get('/user/connections', userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        const connectionRequest = await ConnectionRequest.find({
+            $or: [
+                { toUserId: loggedInUser._id, status: "accepted" },
+                { fromUserId: loggedInUser._id, status: "accepted" }
+            ]
+        }).populate("fromUserId", USER_SAFE_DATA).populate("toUserId", USER_SAFE_DATA);
+
+        const data = connectionRequest.map((row) => {
+            if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+                return row.toUserId;
+            }
+            return row.fromUserId;
+        });
+
+        res.json({ data });
+
+    } catch (err) {
+        res.status(400).send('ERROR : ' + err.message);
+    }
+})
+
 
 module.exports = requestRouter;
